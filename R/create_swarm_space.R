6324cad5a7e22752ab788ad16177adef3d285c24
marinapapa <marina.vp@outlook.com>
2022-12-29 18:23:56 -0300

    vignette 2 for swarm space added

--- R/create_swarm_space.R
+++ R/create_swarm_space.R
@@ -5,6 +5,7 @@
 #' @param event_duration_limit Filter out events that are shorter than given duration.
 #' @param tsne_rand_seed Random seed for tsne
 #' @param tsne_perplexity Perplexity parameter for tsne. Usually between 10-50.
+#' @param save_pca_object Whether to save the results of the PCA analysis (instead of just returning the PC1-3 points)
 #' @return the swarm space, x and y coordinates per event of each species
 #' @author Marina Papadopoulou \email{m.papadopoulou.rug@@gmail.com}
 #' @export
@@ -12,23 +13,32 @@ create_swarm_space <- function(metrics_data,
                         space_type = 'pca',
                         event_duration_limit = NA,
                         tsne_rand_seed = NA,
-                        tsne_perplexity = 10
+                        tsne_perplexity = 10,
+                        save_pca_object = FALSE
                         )
 
 {
   if (!(is.na(event_duration_limit)))
   {
+    if (!(any(colnames(metrics_data) == 'event_dur_s'))){
+      stop("A column named 'event_dur_s' is needed to apply an event duration limit.")
+    }
     metrics_data <- metrics_data[metrics_data$event_dur_s > event_duration_limit, ]
   }
 
   metrics_data <- metrics_data[stats::complete.cases(metrics_data),]
-  topca <- metrics_data[, !(names(metrics_data) %in% c(event, event_dur, event_dur_s))]
+  topca <- metrics_data[, !(names(metrics_data) %in% c('event', 'event_dur', 'event_dur_s'))]
 
   if (space_type == 'pca')
   {
-    my.pca <- stats::prcomp(topca[,names(topca) != 'species'], center = TRUE, scale. = TRUE)
-    pca_data <- data.frame(my.pca$species, my.pca$x[,1:3])
-    #save(my.pca, file = '../data/pca_res.Rdata')
+    pca_res <- stats::prcomp(topca[,names(topca) != 'species'], center = TRUE, scale. = TRUE)
+    pca_data <- data.frame(topca$species, pca_res$x[,1:3])
+    colnames(pca_data) <- c('species', 'PC1', 'PC2', 'PC3')
+    if (save_pca_object)
+    {
+      save(pca_res, file = '../data/pca_res.Rdata')
+    }
+
     return(pca_data)
   }
 
@@ -36,9 +46,9 @@ create_swarm_space <- function(metrics_data,
   {
     if (is.na(tsne_rand_seed)) {stop('Please give a random seed for the tsne analysis.')}
     set.seed(tsne_rand_seed)
-    tsne <- Rtsne::Rtsne(topca, dims = 2, perplexity = tsne_perplexity, verbose = TRUE, max_iter = 10000)
+    tsne <- Rtsne::Rtsne(topca[,names(topca) != 'species'], dims = 2, perplexity = tsne_perplexity, verbose = TRUE, max_iter = 10000)
     tsnedata <- data.frame(topca$species, tsne$Y[,1:2])
-    colnames(tsnedata) <- c('species', 'x', 'y')
+    colnames(tsnedata) <- c('species', 'X', 'Y')
     return(tsnedata)
   }
 }
@@ -56,32 +66,25 @@ create_swarm_space <- function(metrics_data,
 expand_pca_swarm_space <- function(metrics_data,
                         pca_space,
                         pca_data,
-                        event_duration_limit
+                        event_duration_limit = NA
 )
 {
   if (!(is.na(event_duration_limit)))
   {
+    if (!(any(colnames(metrics_data) == 'event_dur_s'))){
+      stop("A column named 'event_dur_s' is needed to apply an event duration limit.")
+    }
     metrics_data <- metrics_data[metrics_data$event_dur_s > event_duration_limit, ]
   }
 
   metrics_data <- metrics_data[stats::complete.cases(metrics_data),]
-  topca <- metrics_data[, !(names(metrics_data) %in% c(species, event, event_dur, event_dur_s))]
+  topca <- metrics_data[, !(names(metrics_data) %in% c('species', 'event', 'event_dur', 'event_dur_s'))]
 
-  newpca<- as.data.frame(stats::predict(pca_space, topca))
+  newpca <- as.data.frame(stats::predict(pca_space, topca))
   newpca$species <- metrics_data$species
 
-  pcadata <- pcadata[, c(1,3,4)]
-  newpca <- newpca[, c(10, 1,2)]
-  alldata <- rbind(pcadata, newpca)
+  #pca_data <- pca_data[, names(pca_data) %in% c('species', 'PC1', 'PC2', 'PC3')]
+  newpca <- newpca[, names(newpca) %in% c('species', 'PC1', 'PC2', 'PC3')]
+  alldata <- rbind(pca_data, newpca)
   return(alldata)
 }
-
-
-
-
-
-
-
-
-
-
